46769075d68c4355c0c160fe66a4aec6557453dc
machmaegal <aka.pleat371@passinbox.com>
2024-11-13 18:18:03 +0100

    changes

--- kanban-board/src/components/Displays.jsx
+++ kanban-board/src/components/Displays.jsx
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import taskData from '../assets/mock-data.json';
 
 const Displays = () => {
+	const [tasks, setTasks] = useState(taskData);
+
+	tasks.map((task) => {
+		console.log(task);
+
+		/* return <div className='tasks' key={task.id}>
+			<h1>{task.title}</h1>
+			<h2>{task.priority}</h2>
+			<p>{task.description}</p>
+			<p>{task.status}</p>
+		</div>;
+	{ task.status === 'Done' && document.querySelector('#done').appendChild(task); } */
+	});
+
 	return (
 		<div className='display'>
-			<section id="toDo"></section>
-			<section id="inProgress"></section>
+			<section id="todo"></section>
+			<section id="in-progress"></section>
 			<section id="done"></section>
 		</div>
 	);
